e35705dd32e177556bbbc199f50e35ef4adfba43
Ires <ik.lejava@gmail.com>
2024-08-16 17:09:57 +0400

    PreFinal

--- src/main/java/com/example/blog/model/Post.java
+++ src/main/java/com/example/blog/model/Post.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "post")
 public class Post {
 
     @Id
--- src/main/java/com/example/blog/model/User.java
+++ src/main/java/com/example/blog/model/User.java
@@ -59,3 +59,4 @@ public class User {
         this.roles = roles;
     }
 }
+
