7523c22bde303aa74ea41f2bb298b76999b31f83
Ires <ik.lejava@gmail.com>
2024-08-24 21:04:50 +0400

    blog-7.0

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -3,7 +3,8 @@ package com.example.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.blog")
+
 public class BlogApplication {
 
     public static void main(String[] args) {
